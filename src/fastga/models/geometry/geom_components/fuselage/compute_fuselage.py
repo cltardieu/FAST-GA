819af01566b760838c11857cbae9f014c0ed1906
cltardieu <92374376+cltardieu@users.noreply.github.com>
2021-10-12 14:39:24 +0200

    First commit
    Changed a commentary to visualize the fork

--- src/fastga/models/geometry/geom_components/fuselage/compute_fuselage.py
+++ src/fastga/models/geometry/geom_components/fuselage/compute_fuselage.py
@@ -1,5 +1,5 @@
 """
-    Estimation of geometry of fuselage part A - Cabin (Commercial)
+    Estimation of geometry of fuselage part A - Cabin (General Aviation)
 """
 
 #  This file is part of FAST : A framework for rapid Overall Aircraft Design
